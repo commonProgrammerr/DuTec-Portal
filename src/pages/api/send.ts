a3ecc39799ef140de7ef9b9b5c32fc4f9e5686fc
Andre Escorel <agver@ecomp.poli.br>
2021-09-26 01:42:02 -0300

    fix broken site on faield on send a email

--- src/pages/api/send.ts
+++ src/pages/api/send.ts
@@ -1,82 +1,86 @@
-
-import { NextApiHandler } from 'next/types'
-import nodemailer from 'nodemailer'
-import dotenv from 'dotenv'
+import { NextApiHandler } from "next/types";
+import nodemailer, { Transporter } from "nodemailer";
+import dotenv from "dotenv";
 
 export type APIResponse = {
-  status: string
-  message?: string
-  name?: string
+  status: string;
+  message?: string;
+  name?: string;
+};
+
+dotenv.config();
+
+function getMailMensage(ctx) {
+  return {
+    title: "Requisição de testes",
+    body: `${ctx.name} requisitou testar o software do kit Dutec.
+    O estudante percence a instituição '${ctx.escola}' e mora em ${ctx.cidade}-${ctx.UF}.
+    Email para contato: ${ctx.email}`,
+  };
 }
 
-dotenv.config()
-
+async function sendMail(emailTransponder: Transporter, mensage) {
+  return new Promise((resolve, reject) => {
+    emailTransponder.sendMail(mensage, (err, info) => {
+      if (err) {
+        const { message, name } = err;
+        console.error(name, ": ", message);
+        reject(err);
+      } else {
+        resolve(info);
+      }
+    });
+  });
+}
 
 const sendEmail: NextApiHandler<APIResponse> = async (req, res) => {
   try {
-    const { name, email, UF, cidade, escola } = req.body
+    const { name, email, UF, cidade, escola } = req.body;
     
     const mailTransporter = nodemailer.createTransport({
       port: 465,
       host: "smtp.gmail.com",
       auth: {
-        user: 'dutecbot@gmail.com',
+        user: "dutecbot@gmail.com",
         pass: process.env.password,
       },
       secure: true,
-    })
-    const mail = {
-      title: 'Requisição de testes',
-      body: `${name} requisitou testar o software do kit Dutec.
-      O estudante percence a instituição '${escola}' e mora em ${cidade}-${UF}.
-      Email para contato: ${email}`
-    }
-    
-    const mailData = {
-      from: 'dutecbot@gmail.com',
-      to: 'desenvolvimento@dulino.com.br',
+    });
+    const mail = getMailMensage({
+      name,
+      email,
+      UF,
+      cidade,
+      escola,
+    });
+
+    const mensage = {
+      from: "dutecbot@gmail.com",
+      to: "desenvolvimento@dulino.com.br",
       subject: `Requisitção de teste, por ${name}`,
       text: mail.body,
-      html: `<h1>${mail.title}</h1>\n<p>${mail.body}</p>`
-    }
-    
-    return new Promise((resolve, reject) => {
-      mailTransporter.once('error', reject)
-      try {
-        return mailTransporter.sendMail(mailData, (err, info) => {
-          if (err) {
-            const { message, name } = err
-            res.status(502).json({
-              status: 'send error',
-              message,
-              name
-            })
-            console.log(name, ': ', message)
-            reject(err)
-          }
-          else {
-            resolve(res.status(200).json({ status: 'Done! ' }))
-            console.log(info)
-          }
-        })
-      } catch (err) {
+      html: `<h1>${mail.title}</h1>\n<p>${mail.body}</p>`,
+    };
 
-        const { message, name } = err
-        console.log(name, ': ', message)
-        
-        res.status(500).json({ status: 'internal error', message, name })
-        
-        return reject(err)
-      }
-    })
-  } catch (error) {
-    
-    const { message, name } = error
-    console.log(name, ': ', message)
-    
-    res.status(500).json({ status: 'server error', message, name })
-    return new Promise<void>((resolve, reject) => reject(error))
+    sendMail(mailTransporter, mensage)
+      .then((info) => {
+        res.status(200).json({ status: "Done! " });
+        console.log(info);
+      })
+      .catch((err) => {
+        const { message, name } = err;
+        res.status(502).json({
+          status: "send error",
+          message,
+          name,
+        });
+      });
+  } catch (err) {
+    const { message, name } = err;
+    console.log(name, ": ", message);
+
+    res.status(500).json({ status: "internal error", message, name });
   }
-}
+};
 
-export default sendEmail
+export default sendEmail;
